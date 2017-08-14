71a16929a5e085ff733d330ab64e2a8df5a5df48
Rajat Harlalka <rajatharlalka@Rajats-MacBook-Pro.local>
2017-08-14 16:39:07 +0530

    first commit

---
+++ routes/todos.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://rajat:rajat@ds151927.mlab.com:51927/meantodos_rajat_dev',['todos']);
+
+router.get('/todos', function(req, res, next){
+	db.todos.find(function(err, todos){
+		if(err){
+			res.send(err);
+		}
+		else{
+			res.json(todos);
+		}
+	});
+});
+
+//Get Single Todos
+router.get('/todo/:id', function(req, res, next){
+	db.todos.findOne({
+		_id: mongojs.ObjectId(req.params.id)
+	}, function(err, todo){
+		if(err){
+			res.send(err);
+		}
+		else{
+			res.json(todo);
+		}
+	});
+});
+
+
+//Save ToDo
+router.post('/todo', function(req, res, next){
+	var todo = req.body;
+	if(!todo.text || !(todo.isComplicated + '')){
+		res.status(400);
+		res.json({
+			"error": "Invalid Data"
+		});
+	}else
+	{
+		db.todos.save(todo, function(err, result){
+			if(err){
+				res.send(err);
+			}
+			else{
+				res.json(result);
+			}
+		});
+	}
+
+});
+
+
+//Update ToDo
+router.put('/todo/:id', function(req, res, next){
+    var todo = req.body;
+    var updObj = {};
+    
+    if(todo.isCompleted){
+       updObj.isCompleted = todo.isCompleted;
+    }
+    
+    if(todo.text){
+        updObj.text = todo.text;
+    }
+    
+    if(!updObj){
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    } else {
+        db.todos.update({
+            _id: mongojs.ObjectId(req.params.id)
+        },updObj, {}, function(err, result){
+            if(err){
+                res.send(err); 
+            } else {
+                res.json(result);
+            }
+        });
+    }
+});
+
+
+//Delete ToDos
+router.delete('/todo/:id', function(req, res, next){
+    db.todos.remove({
+        _id: mongojs.ObjectId(req.params.id)
+    },'', function(err, result){
+        if(err){
+            res.send(err); 
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+
+
+
+
+module.exports = router;
